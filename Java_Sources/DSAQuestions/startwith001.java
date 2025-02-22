17d19a675ec601a1f09776ebb133caf6a58311ae
Bahir Hussain <mbahirh624@gmail.com>
2025-02-22 15:20:15 +0300

    Added

--- Java_Sources/DSAQuestions/startwith001.java
+++ Java_Sources/DSAQuestions/startwith001.java
@@ -5,8 +5,8 @@ import java.util.Scanner;
 public class startwith001 {
     public static void main(String[] args){
         Scanner scn = new Scanner(System.in);
-        int num = scn.nextInt().trim();
+        int num = scn.nextInt();
 
-        if(num.)
+
     }
 }
