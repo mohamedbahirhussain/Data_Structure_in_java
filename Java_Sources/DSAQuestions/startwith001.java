ac893c680a50de216159c1e9e89d5445796f2ee1
Bahir Hussain <mbahirh624@gmail.com>
2025-02-23 18:46:28 +0300

    Update

---
+++ Java_Sources/DSAQuestions/startwith001.java
@@ -0,0 +1,12 @@
+package DSAQuestions;
+
+import java.util.Scanner;
+
+public class startwith001 {
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+        int num = scn.nextInt().trim();
+
+        if(num.)
+    }
+}
