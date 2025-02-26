4d1a32a2d84a5c90e5c6bd781eb3088308459167
Bahir Hussain <mbahirh624@gmail.com>
2025-02-26 20:54:01 +0300

    added

--- Java_Sources/DSAQuestions/startwith001.java
+++ Java_Sources/DSAQuestions/startwith001.java
@@ -3,10 +3,18 @@ package DSAQuestions;
 import java.util.Scanner;
 
 public class startwith001 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         int num = scn.nextInt();
+        scn.close();
 
+        // Convert the number to a string to check if it starts with "001"
+        String numStr = String.valueOf(num);
 
+        if (numStr.startsWith("001")) {
+            System.out.println("The number starts with 001");
+        } else {
+            System.out.println("The number does not start with 001");
+        }
     }
 }
